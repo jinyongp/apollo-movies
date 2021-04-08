99ca4b5aa7d2a310070e2fec0e6f83b4512de1c1
jinyongp <dev.jinyongp@gmail.com>
2021-04-09 01:11:08 +0900

    Add routers: Home, Detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import { HashRouter as Router, Route } from "react-router-dom";
+import { Home, Detail } from "../routes";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route path="/" component={Home} exact />
+      <Route path="/:id" component={Detail} />
+    </Router>
+  );
 }
 
 export default App;
